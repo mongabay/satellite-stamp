ac9eb78ec2efbb47dcf6f8332218bedf01ac15e1
Clément Prod'homme <contact@clementprodhomme.fr>
2020-05-11 11:09:52 +0100

    Style the modal

--- components/icons/index.js
+++ components/icons/index.js
@@ -8,7 +8,13 @@ const Icons = () => (
     xmlns="http://www.w3.org/2000/svg"
     xlink="http://www.w3.org/1999/xlink"
   >
-    <defs />
+    <defs>
+      <symbol id="icon-close" viewBox="0 0 9 9">
+        <title>Close</title>
+        <path d="M7.41.839l1.087 1.087-7.07 7.071L.338 7.91z" />
+        <path d="M8.498 7.91L7.41 8.998.339 1.927 1.427.839z" />
+      </symbol>
+    </defs>
   </svg>
 );
 
