e17f309769124ca932d7fa4470be77305e3192fb
Clément Prod'homme <contact@clementprodhomme.fr>
2020-05-11 16:42:22 +0100

    Add the basic layout of the page

---
+++ pages/home/constants.js
@@ -0,0 +1,101 @@
+export const BASEMAPS = {
+  'mongabay-paper': {
+    label: 'Mongabay Paper',
+  },
+  landsat: {
+    label: 'Landsat',
+  },
+  planet: {
+    label: 'Planet',
+  },
+  topography: {
+    label: 'Topography',
+  },
+  'landsat-8': {
+    label: 'Landsat 8',
+  },
+  'sentinel-2': {
+    label: 'Sentinel 2',
+  },
+};
+
+export const CONTEXTUAL_LAYERS = {
+  labels: {
+    label: 'Labels',
+  },
+  'admin-boundaries': {
+    label: 'Admin boundaries',
+  },
+  roads: {
+    label: 'Roads',
+  },
+  'elevation-lines': {
+    label: 'Elevation lines',
+  },
+  hillshade: {
+    label: 'Hillshade',
+  },
+  water: {
+    label: 'Water',
+  },
+};
+
+export const DATA_LAYERS = {
+  'tree-cover-loss': {
+    label: 'Tree cover loss',
+  },
+  glad: {
+    label: 'Deforestation alerts (GLAD)',
+  },
+  'tree-cover': {
+    label: 'Tree cover',
+  },
+  'primary-forests': {
+    label: 'Primary forests',
+  },
+  'south-america-tree-cover-height': {
+    label: 'South America tree cover height',
+  },
+  'forest-canopy-height': {
+    label: 'Global forest canopy height',
+  },
+  'land-cover': {
+    label: 'Global land cover',
+  },
+  'tree-plantations': {
+    label: 'Tree plantations',
+  },
+  'protected-areas': {
+    label: 'Protected areas',
+  },
+  'urban-built-up-area': {
+    label: 'Urban built-up Area',
+  },
+  population: {
+    label: 'Population (grid, 250 m)',
+  },
+  'population-density': {
+    label: 'Population density (grid, 1 km)',
+  },
+  roads: {
+    label: 'Roads',
+  },
+  'logging-concessions': {
+    label: 'Logging concessions',
+  },
+  'mining-concessions': {
+    label: 'Mining concessions',
+  },
+  'oil-palm-concessions': {
+    label: 'Oil palm concessions',
+  },
+  'wood-fiber-concessions': {
+    label: 'Wood fiber concessions by type',
+  },
+  'palm-oil-mills': {
+    label: 'Palm oil mills',
+  },
+  'indigenous-territories': {
+    label: 'Indigenous territories',
+  },
+};
