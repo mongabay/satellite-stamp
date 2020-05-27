52c653daa1fda1cfba67f590d589b53086f0e058
Clément Prod'homme <contact@clementprodhomme.fr>
2020-05-27 12:23:22 +0100

    Fix a bug where the state couldn't be restored
    The bug would only happen in production as the initial state of the store would be empty.

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
-import withRedux from 'next-redux-wrapper';
 
-import createStore from 'lib/store';
-import { updateRoute } from 'modules/routing';
+import store from 'lib/store';
 
 import 'css/index.scss';
 
-const SatelliteStampApp = ({ Component, pageProps, store }) => {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  );
-};
-
-SatelliteStampApp.getInitialProps = async ({ Component, router, ctx }) => {
-  const { asPath } = router;
-  const { req, query, store } = ctx;
-
-  const pathname = req ? asPath : ctx.asPath;
-
-  // We save the route in Redux
-  if (pathname) {
-    store.dispatch(updateRoute({ pathname, query }));
-  }
-
-  const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
-
-  return { pageProps };
-};
+const SatelliteStampApp = ({ Component, pageProps }) => (
+  <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
+);
 
 SatelliteStampApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.any.isRequired,
-  store: PropTypes.any.isRequired,
+  pageProps: PropTypes.any,
 };
 
-export default withRedux(createStore)(SatelliteStampApp);
+SatelliteStampApp.defaultProps = {
+  pageProps: {},
+};
+
+export default SatelliteStampApp;
