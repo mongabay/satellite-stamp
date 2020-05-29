82cd0c052b595cbc26508da3ac79c26e8bc8702e
clementprdhomme <clementprdhomme@users.noreply.github.com>
2020-05-29 17:07:12 +0000

    deploy: 04706246c916412cbcb8f9ec9ffde5470b884c62

---
+++ _next/static/31BfsaV496YeCGMzKYfLX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/home/component":[a],"/index":[a]}}("static\u002Fchunks\u002F18cd9d578ef88b237f5674f2b8d431653c1edde6.e6a9f7c83225eb3a5457.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
