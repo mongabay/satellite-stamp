6a4086f7793d1256a0bced752419e445e70e4259
Bárbara Chaves <barbarabarcellos90@gmail.com>
2024-02-18 14:11:15 +0100

    Feature/mp 202 update style (#30)
    * Update style

--- components/header/constants.js
+++ components/header/constants.js
@@ -3,28 +3,26 @@ export const COLORS = ['#03755E', '#184B95', '#A01200', '#7E56D8'];
 export const TOOLS = [
   {
     id: 'earth-atlas',
-    name: 'Earth Atlas',
     color: COLORS[1],
+    name: 'Earth Atlas',
     url: 'https://maps.mongabay.com',
   },
   {
     id: 'fire-weather-almanac',
-    name: 'Fire Weather Almanac',
     color: COLORS[2],
-    image: '/images/fire-tool.png',
+    name: 'Fire Weather Almanac',
     url: 'https://charts.mongabay.com',
   },
   {
     id: 'reforestation-app',
-    title: 'Reforestation App',
     color: COLORS[0],
     name: 'Reforestation App',
     url: 'https://reforestation.app',
   },
   {
     id: 'trade-flow-map',
-    name: 'Trade Flow Map',
     color: COLORS[3],
+    name: 'Trade Flow Map',
     url: 'https://chains.mongabay.com',
   },
 ];
--- components/header/menu/index.js
+++ components/header/menu/index.js
@@ -40,7 +40,7 @@ const HeaderMenu = () => {
             <span className="sr-only">close navigation menu</span>
             <Icon name="menu-close" className="header__menu__content__wrap__close-btn__icon" />
           </button>
-          <p className="header__menu__content__wrap__title">Data Journalism Tools</p>
+          <p className="header__menu__content__wrap__title">Mongabay Data Studio</p>
           <ul className="space-y-6">
             {TOOLS.map(({ name, url, id, color }) => (
               <li key={name}>
@@ -57,19 +57,12 @@ const HeaderMenu = () => {
               </li>
             ))}
           </ul>
-          <div className="header__menu__content__wrap__buttons">
-            {/* TODO: add lights */}
+          <div className="header__menu__content__wrap__button">
             <a
-              href="mailto:info@mongabay.org"
-              className="btn btn-dark text-white bg-dark-gray shadow-none"
+              href="https://www.mongabay.com/contact/"
+              className="btn btn-dark text-white bg-black shadow-none"
             >
               Get in touch
-            </a>
-            <a
-              href="https://mongabay.org/"
-              className="btn btn-light text-dark-gray border border-gray-dark shadow-none"
-            >
-              Find out more
             </a>
           </div>
         </div>
